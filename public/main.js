8e5df2ef4f787402bef138b1c81a9a7d3fdc1cbc
Chris Terrazas <chris.terrazas@gmail.com>
2018-11-05 22:42:49 -0500

    everything works - done with expolrer

--- public/main.js
+++ public/main.js
@@ -1,5 +1,48 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+// variables ---------------------------------------------
+const searchBox = document.querySelector('.search-box')
+const searchButton = document.querySelector('.search-button')
+const displayCity = document.querySelector('.display-city')
+const displayTemp = document.querySelector('.display-temp')
+const displayWindSpeed = document.querySelector('.display-wind-speed')
+const displayWeather = document.querySelector('.display-weather')
+let weatherApiData = ''
+
+const fetchData = (where) => {
+  let response = fetch(`https://api.openweathermap.org/data/2.5/weather?${where}&units=imperial&appid=35c1d9a3bea84a41342db0b75d057c10`)
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json()
+      } else {
+        wrongLocation()
+      }
+    }).then((json) => {
+      weatherApiData = json
+      displayData()
+    })
 }
 
+const displayData = () => {
+  displayCity.textContent = weatherApiData.name
+  displayTemp.textContent = weatherApiData.main.temp
+  displayWindSpeed.textContent = weatherApiData.wind.speed
+  displayWeather.textContent = weatherApiData.weather[0].description
+}
+
+const wrongLocation = () => {
+  displayCity.textContent = 'Invalid City - Try again'
+  displayTemp.textContent = ''
+  displayWindSpeed.textContent = ''
+  displayWeather.textContent = ''
+}
+
+const main = () => {
+  searchButton.addEventListener('click', () => {
+    let where = searchBox.value
+    if (isNaN(where)) {
+      fetchData(`q=${where}`)
+    } else {
+      fetchData(`zip=${where}`)
+    }
+  })
+}
 document.addEventListener('DOMContentLoaded', main)
